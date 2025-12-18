125892a271e899125bdb060f844d3ae7c06a8e10
GabrielaAmabily <amabilygabriela56@gmail.com>
2025-12-18 17:06:09 -0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 import 'classeItem.dart';
 import 'backendFalso.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 //usar acima 
 
 void main() {
-  runApp(const MaterialApp(home: Text('Hello World!')));
+  runApp(const AsiCoffeeApp());
+}
+
+class AsiCoffeeApp extends StatelessWidget {
+  const AsiCoffeeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'AsiCoffee',
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+        textTheme: GoogleFonts.poppinsTextTheme(),
+      ),
+      //home: const HomePage(), //// fazerrr
+    );
+  }
 }
