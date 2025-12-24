043d7375ce4d24e1d706b4c4eb8cc4a3843b8144
GabrielaAmabily <amabilygabriela56@gmail.com>
2025-12-24 20:06:20 -0300

    imagens corretas

--- lib/backend_falso.dart
+++ lib/backend_falso.dart
@@ -4,31 +4,31 @@ final List<Item> itens = [
   Item(
     nome: 'café expresso',
     preco: 8.0,
-    imageUrl: 'https://images.unsplash.com/photo-1511920170033-f8396924c348',
+    imageUrl: 'https://images.unsplash.com/photo-1509042239860-f550ce710b93',
     categoria: 'Café',
   ),
   Item(
     nome: 'Café com Leite',
     preco: 5.0,
-    imageUrl: 'https://images.unsplash.com/photo-1511920170033-f8396924c348',
+    imageUrl: 'https://images.unsplash.com/photo-1521302080334-4bebac2763a6',
     categoria: 'Café',
   ),
   Item(
     nome: 'Capuccino',
     preco: 12.00,
-    imageUrl: 'https://images.unsplash.com/photo-1511920170033-f8396924c348',
+    imageUrl: 'https://images.unsplash.com/photo-1517256064527-09c73fc73e38',
     categoria: 'Café',
   ),
   Item(
     nome: 'Croissant',
     preco: 8.00,
-    imageUrl: 'https://images.unsplash.com/photo-1511920170033-f8396924c348',
+    imageUrl: 'https://images.unsplash.com/photo-1549931319-a545dcf3bc73',
     categoria: 'Salgado',
   ),
   Item(
     nome: 'Bolo de Chocolate',
     preco: 9.50,
-    imageUrl: 'https://images.unsplash.com/photo-1511920170033-f8396924c348',
+    imageUrl: 'https://images.unsplash.com/photo-1606313564200-e75d5e30476c',
     categoria: 'Doce',
   ),
 
