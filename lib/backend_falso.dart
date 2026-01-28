54f2f09d6b0118f364bab3486196b5297408f217
GabrielaAmabily <amabilygabriela56@gmail.com>
2026-01-28 09:51:36 -0300

    usando enum para as categorias

--- lib/backend_falso.dart
+++ lib/backend_falso.dart
@@ -1,35 +1,35 @@
-import 'package:first_app/classe_item.dart';
+import 'classe_item.dart';
 
 final List<Item> itens = [
   Item(
     nome: 'café expresso',
     preco: 8.0,
     imageUrl: 'https://images.unsplash.com/photo-1509042239860-f550ce710b93',
-    categoria: 'Café',
+    categoria: Categoria.cafes,
   ),
   Item(
     nome: 'Café com Leite',
     preco: 5.0,
     imageUrl: 'https://images.unsplash.com/photo-1521302080334-4bebac2763a6',
-    categoria: 'Café',
+    categoria: Categoria.cafes,
   ),
   Item(
     nome: 'Capuccino',
     preco: 12.00,
     imageUrl: 'https://images.unsplash.com/photo-1517256064527-09c73fc73e38',
-    categoria: 'Café',
+    categoria: Categoria.cafes,
   ),
   Item(
     nome: 'Croissant',
     preco: 8.00,
     imageUrl: 'https://images.unsplash.com/photo-1549931319-a545dcf3bc73',
-    categoria: 'Salgado',
+    categoria: Categoria.salgados,
   ),
   Item(
     nome: 'Bolo de Chocolate',
     preco: 9.50,
     imageUrl: 'https://images.unsplash.com/photo-1606313564200-e75d5e30476c',
-    categoria: 'Doce',
+    categoria: Categoria.bolos,
   ),
 
 ];
--- lib/classe_item.dart
+++ lib/classe_item.dart
@@ -1,8 +1,10 @@
+enum Categoria { cafes, sucos, refrigerante, salgados, bolos, doces, lanches }
+
 class Item {
   final String nome;
   final double preco;
   final String imageUrl;
-  final String categoria;
+  final Categoria categoria;
   bool favorito;
 
   Item({
